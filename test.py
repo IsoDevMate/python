d2e39d648a15586ad2e4a081393c5b972be4860f
IsoDevMate <oumaoduor5827@gmail.com>
2023-06-29 10:30:05 +0300

    learnt print function

---
+++ test.py
@@ -0,0 +1,88 @@
+
+print('nameis baro.', end=" ")
+print('nameis baro')
+print("my", "name","is","Kabori", sep="_")
+
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+# Sample Solution
+
+###################
+print("original version:")
+###################
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+###################
+print("with fewer 'print()' invocations:")
+###################
+print("    *\n   * *\n  *   *\n *     *\n***   ***")
+print("  *   *\n  *   *\n  *****")
+###################
+print("higher:")
+###################
+print("        *")
+print("       * *")
+print("      *   *")
+print("     *     *")
+print("    *       *")
+print("   *         *")
+print("  *           *")
+print(" *             *")
+print("******     ******")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *******")
+###################
+print("doubled:")
+###################
+print("        *        "*2)
+print("       * *       "*2)
+print("      *   *      "*2)
+print("     *     *     "*2)
+print("    *       *    "*2)
+print("   *         *   "*2)
+print("  *           *  "*2)
+print(" *             * "*2)
+print("******     ******"*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *******     "*2)
+
+print("my solution\n")
+
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+
+arrow = "    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****"
+print(arrow * 2)
+
+
+arrow = "    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****"
+duplicated_arrows = arrow + arrow
+print(duplicated_arrows)
+
+
